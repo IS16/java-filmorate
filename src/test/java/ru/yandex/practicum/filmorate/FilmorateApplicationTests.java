30b5b4f79d709afa6eaec7ef3de76424eeb08dd1
IS16 <sarkisov775@yandex.ru>
2023-09-17 05:18:19 +0300

    clear unused imports

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,16 +1,7 @@
 package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.json.*;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 @SpringBootTest
 class FilmorateApplicationTests {
