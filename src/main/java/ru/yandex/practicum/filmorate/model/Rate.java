2626e6b9f66826b56f1570e6cdaa1dc4577039d8
IS16 <sarkisov775@yandex.ru>
2023-10-15 20:10:45 +0300

    refactor style

--- src/main/java/ru/yandex/practicum/filmorate/model/Rate.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Rate.java
@@ -7,7 +7,7 @@ public class Rate {
     private Integer id;
     private String name = "";
 
-    public Rate (int id, String name) {
+    public Rate(int id, String name) {
         this.id = id;
         this.name = name;
     }
